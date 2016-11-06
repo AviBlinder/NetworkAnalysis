d0065172755b2ea8c187f5b90546bf5c3e126865
Avi Blinder <aviblinder@gmail.com>
2016-11-06 16:48:00 +0200

    plots

--- scripts/make_tables.r
+++ scripts/make_tables.r
@@ -1,56 +1,46 @@
 #ftp://cran.r-project.org/pub/R/web/packages/gridExtra/vignettes/tableGrob.html
 library(grid)
 library(gridExtra)
-
+library(formattable)
+png(filename = "./figures/Top Reviewed Restaurants.png")
 DT <- input_business[,2:3]
 row.names(DT) <- NULL
-
+names(DT) <- c("Name","Number of Reviews")
 t1 <- ttheme_default()
 grid.table(DT[1:10,], theme = t1)
+dev.off()
 
 ###############################################################################
+Sys.setlocale("LC_ALL","C")
+png(filename = "./figures/Top Users by Number of Reviewes.png")
 DT <- reviews_number1
-row.names(DT) <- NULL
+
 names(DT) <-  c("User","Yelper Since","N. of Friends","Elite Years",
                 "Number of Reviews on Top Restaurants","Total Reviews","Fans",
                 "Average Stars")
 
+DT <- DT[order(-DT$`N. of Friends`),]
+row.names(DT) <- NULL
 
-t1 <- ttheme_default()
-grid.table(DT[,c(1:3,6)], theme = t1)
-
-
-
-###############################################################################
-t1 <- ttheme_default(core=list(
-  fg_params=list(fontface=c(rep("plain", 8), "bold.italic")),
-  bg_params = list(fill=c(rep(c("grey95", "grey90"),
-                              length.out=8), "#6BAED6"),
-                   alpha = rep(c(1,0.5), each=5))
+formattable(DT[1:15,], list(
+  `N. of Friends` = color_tile("white","green"),
+  'Number of Reviews on Top Restaurants' = color_bar(color = "lightgray")
 ))
 
-grid.table(DT[1:10, 1:8], theme = t1)
-t1 <- ttheme_default()
-grid.table(DT[1:10, 1:8], theme = t1)
+
+dev.off()
 
 ###############################################################################
-business_sna
-DT <- business_sna
-row.names(DT) <- NULL
-dim(DT)
-t1 <- ttheme_default()
-t1 <- ttheme_default(core=list(
-  fg_params=list(cex = 0.8,hjust=1, x=0.5,fontface=c(rep("plain", 6), "bold.italic"))),
-  colhead = list(fg_params=list(cex = 0.8))
-)
-#  bg_params = list(fill=c(rep(c("grey95", "grey90"),
-#                              length.out=6), "#6BAED6"),
-#                   alpha = rep(c(1,0.5), each=5))
+head(business_sna)
+row.names(business_sna) <- NULL
 
 
-png(filename = "business_sna.png")
+formattable(business_sna[1:15,] )
+#            , list(
+#  `vcounts` = color_tile("white","green"),
+#  'Number of Reviews on Top Restaurants' = color_bar(color = "lightgray")
+#))
 
-g <- grid.table(DT, theme = t1)
 
 
 dev.off()
