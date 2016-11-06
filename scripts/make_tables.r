5ec7261b0abe0db68be8cb6a1293caa48925850f
Avi Blinder <aviblinder@gmail.com>
2016-11-06 16:54:54 +0200

    plots

--- scripts/make_tables.r
+++ scripts/make_tables.r
@@ -2,6 +2,36 @@
 library(grid)
 library(gridExtra)
 library(formattable)
+###############################################################################
+png(filename = "./figures/Degrees Distributions.png")
+degree.g <- degree(Y_graph)
+degrees_table <- as.data.frame(table(degree.g))
+names(degrees_table) <- c("Degree","Frequency")
+row.names(degrees_table) <- NULL
+degrees_table$Degree <- as.integer(degrees_table$Degree)
+
+p1 <- ggplot(degrees_table, aes(x = Degree, y = Frequency))
+plot1 <- p1 +  geom_point(aes(color = Degree)) +
+  geom_vline(xintercept = 5, color=colors()[230],show.legend = FALSE) +
+  labs(x="Degree Distribution",
+       y = "Frequency") +
+  ggtitle("Degrees Distribution")
+
+
+p1 <- ggplot(degrees_table, aes(x = log10(Degree), y = log10(Frequency)))
+plot2 <- p1 +  geom_point(aes(color = log10(Degree),show.legend = FALSE)) +
+  geom_vline(xintercept = log10(5), color=colors()[230],show.legend = FALSE) +
+  labs(x="Log10 - Degree Distribution",
+       y = "Log10 - Frequency")  +
+  ggtitle("Log10 of Degrees Distribution")
+
+require(gridExtra)
+grid.arrange(plot1, plot2, ncol=2)
+
+dev.off()
+
+
+###############################################################################
 png(filename = "./figures/Top Reviewed Restaurants.png")
 DT <- input_business[,2:3]
 row.names(DT) <- NULL
