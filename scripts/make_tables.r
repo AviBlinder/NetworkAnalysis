e5f34704df1b7166743ab7af0228f202f1fcbfd9
Avi Blinder <aviblinder@gmail.com>
2016-11-06 08:26:22 +0200

    minor fixes to plots

--- scripts/make_tables.r
+++ scripts/make_tables.r
@@ -1,26 +1,27 @@
 #ftp://cran.r-project.org/pub/R/web/packages/gridExtra/vignettes/tableGrob.html
-DT <- reviews_number1
 library(grid)
 library(gridExtra)
-colnames(DT)[1] <- "User"
-grid.draw(tableGrob(DT,
-           cols = colnames(DT)[1:4], show.box = FALSE,
-#                    name="Users Features",
-                    separator="blue",
-                    padding.v = unit(10, "mm"),
-                    gpar.coretext = gpar(col = "orange", cex = 0.9),
-                    padding.h = unit(4, "mm"),
-                    gpar.corefill = gpar(fill = "white", col = "green")))
+
+DT <- input_business[,2:3]
+row.names(DT) <- NULL
+
+t1 <- ttheme_default()
+grid.table(DT[1:10,], theme = t1)
+
+###############################################################################
+DT <- reviews_number1
+row.names(DT) <- NULL
+names(DT) <-  c("User","Yelper Since","N. of Friends","Elite Years",
+                "Number of Reviews on Top Restaurants","Total Reviews","Fans",
+                "Average Stars")
 
 
-dev.off()
-d <- head(iris, 3)
-g <- tableGrob(d)
-grid.newpage()
-grid.draw(g)
-dev.list()
-?gtable_table
+t1 <- ttheme_default()
+grid.table(DT[,c(1:3,6)], theme = t1)
 
+
+
+###############################################################################
 t1 <- ttheme_default(core=list(
   fg_params=list(fontface=c(rep("plain", 8), "bold.italic")),
   bg_params = list(fill=c(rep(c("grey95", "grey90"),
